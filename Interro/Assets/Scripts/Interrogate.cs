f56dff20f325550507ad046c84e30ba0242af866
Canopenerdude <44471513+Canopenerdude@users.noreply.github.com>
2024-11-09 17:31:01 -0500

    Made the objects do the talkies

--- Interro/Assets/Scripts/Interrogate.cs
+++ Interro/Assets/Scripts/Interrogate.cs
@@ -4,40 +4,67 @@ using UnityEngine;
 
 public class Interrogate : MonoBehaviour
 {
+    public AudioSource rockLine;
+    public AudioSource lampLine;
+    public AudioSource carLine;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rockLine = GetComponent<AudioSource>();
+        lampLine = GameObject.Find("LampLine").GetComponent<AudioSource>();
+        carLine = GameObject.Find("CarLine").GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (rockCollided == true)
+        {
+            rockCollided = false;
+        }
+
+        if (lampCollided == true)
+        {
+            lampCollided = false;
+        }
+
+        if (carCollided == true)
+        {
+            carCollided = false;
+        }
     }
 
-    public bool rockCollided;
-    public bool lampCollided;
-    public bool carCollided;
+    public  bool rockCollided;
+    public  bool lampCollided;
+    public  bool carCollided;
+    public GameObject Rock;
+    public GameObject Lamp;
+    public GameObject Car;
 
     void OnCollisionEnter(Collision collision)
     {
-        while (collision.gameObject.name == "Rock1")
+        if (collision.gameObject.name == "Rock1")
         {
             Debug.Log("This is a first line!");
+            rockLine.Play(0);
             rockCollided = true;
+            Object.Destroy(Rock);
         }
 
-        while (collision.gameObject.name == "Lamp1")
+        else if (collision.gameObject.name == "Lamp1")
         {
             Debug.Log("This is a second line!");
+            lampLine.Play(0);
             lampCollided = true;
+            Object.Destroy(Lamp);
         }
 
-        while (collision.gameObject.name == "Car1")
+        else if (collision.gameObject.name == "Car1")
         {
             Debug.Log("This is a third line!");
+            carLine.Play(0);
             carCollided = true;
+            Object.Destroy(Car);
         }
     }
 }
