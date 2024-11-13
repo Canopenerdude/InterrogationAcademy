0d9cff3c6e3581b553ab37a9953f3d2c53af2318
Canopenerdude <44471513+Canopenerdude@users.noreply.github.com>
2024-11-13 18:00:36 -0500

    File reorg, text for reset button script

---
+++ Interro/Assets/Scripts/FloatingText.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    Transform mainCamera;
+    Transform button;
+    Transform worldSpaceCanvas;
+
+    public Vector3 offset;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main.transform;
+        button = transform.parent;
+        worldSpaceCanvas = GameObject.Find("WorldSpaceCanvas").transform;
+        
+        transform.SetParent(worldSpaceCanvas);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.LookRotation(transform.position - mainCamera.transform.position);
+        transform.position = button.position + offset;
+    }
+}
