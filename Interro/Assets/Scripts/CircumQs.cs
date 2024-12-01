9c69f8b88372f1cebad63592a26a23be2698a760
Canopenerdude <44471513+Canopenerdude@users.noreply.github.com>
2024-12-01 16:39:31 -0500

    adding more buttons

---
+++ Interro/Assets/Scripts/CircumQs.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircumQs : MonoBehaviour
+{
+    private GameObject q1;
+    private GameObject q2;
+    private GameObject q3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Interro/Assets/Scripts/Press.cs
+++ Interro/Assets/Scripts/Press.cs
@@ -12,6 +12,10 @@ public class Press : MonoBehaviour
     private Vector3 q1Pos;
     private Vector3 q2Pos;
     private Vector3 q3Pos;
+    
+    private Quaternion q1Rot;
+    private Quaternion q2Rot;
+    private Quaternion q3Rot;
 
     void Start()
     {
@@ -21,15 +25,21 @@ public class Press : MonoBehaviour
         q1Pos = q1.transform.position;
         q2Pos = q2.transform.position;
         q3Pos = q3.transform.position;
+        q1Rot = q1.transform.rotation;
+        q2Rot = q2.transform.rotation;
+        q3Rot = q3.transform.rotation;
     }
     private void OnMouseUpAsButton()
     {
         Debug.Log("Press as button");
         q1.SetActive(true);
         q1.transform.position = q1Pos;
+        q1.transform.rotation = q1Rot;
         q2.SetActive(true);
         q2.transform.position = q2Pos;
+        q2.transform.rotation = q2Rot;
         q3.SetActive(true);
         q3.transform.position = q3Pos;
+        q3.transform.rotation = q3Rot;
     }
 }
